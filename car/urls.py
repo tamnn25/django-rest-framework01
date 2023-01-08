398ce9bcf792af0bb0e1dc045b4dd37c2154f053
tamnn <tamnn@greenglobal.vn>
2023-01-08 23:30:49 +0700

    Training django

--- car/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ListCreateCarView.as_view()),
-    path('<int:pk>', views.UpdateDeleteCarView.as_view()),
-]
